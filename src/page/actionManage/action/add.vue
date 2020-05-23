71872dd1aabc1905efa1cc19aba4205da083f9b6
Quincy <likequincy@outlook.com>
2020-05-23 15:59:26 +0800

    feat: add part of add-action page

--- src/page/actionManage/action/add.vue
+++ src/page/actionManage/action/add.vue
@@ -6,24 +6,21 @@
         .columnContainer
           .header select project
           .list
-            .item(v-for="(i,n) in projectList" :key="n") {{i.title}}
+            .item(v-for="(i,n) in projectList" :key="n" :class="{active:(currentProjectId===i._id)}" @click="handleChangeProject(i)") {{i.title}}
       el-col(:span="8")
         .columnContainer
           .header select group
           .list
-            .item project name
-            .item project name
-            .item project name
+            .item(v-for="(i,n) in groupList" :key="n" :class="{active:(currentGroupId===i._id)}" @click="handleChangeGroup(i)") {{i.name}}
       el-col(:span="8")
         .columnContainer
           .header select api
           .list
-            .item project name
-            .item project name
-            .item project name
+            .item(v-for="(i,n) in apiList" :key="n" :class="{active:(currentApiId===i._id)}" @click="handleChangeApi(i)") {{i.name}}
 
   el-card
     p this is a default page
+    p {{apiDetail}}
 
 
 </template>
@@ -36,16 +33,49 @@ import { Vue, Component } from 'vue-property-decorator'
 })
 export default class extends Vue {
   projectList: any[] = []
+  currentProjectId: string = ''
   groupList: any[] = []
+  currentGroupId: string = ''
   apiList: any[] = []
+  currentApiId: string = ''
+  apiDetail: any = {
+    _id: '',
+    name: '',
+    method: '',
+    url: '',
+    description: '',
+    query: [],
+    path: [],
+    body: [],
+    header: []
+  }
+  handleChangeProject(item: any) {
+    this.currentProjectId = item._id
+    this.getGroupList()
+  }
+  handleChangeGroup(item: any) {
+    this.currentGroupId = item._id
+    this.getApiList()
+  }
+  handleChangeApi(item: any) {
+    this.currentApiId = item._id
+    this.apiDetail = { ...item }
+  }
   async getProjectList() {
     this.projectList = await this.$http.get('/project?size=0')
+    this.handleChangeProject(this.projectList[0])
   }
   async getGroupList() {
-    this.groupList = await this.$http.get('/apiGroup?size=0')
+    this.groupList = await this.$http.get(
+      `/apiGroup?size=0&project=${this.currentProjectId}`
+    )
+    this.handleChangeGroup(this.groupList[0])
   }
   async getApiList() {
-    this.apiList = await this.$http.get('/apiItem')
+    this.apiList = await this.$http.get(
+      `/apiItem?size=0&apiGroup=${this.currentGroupId}`
+    )
+    this.handleChangeApi(this.apiList[0])
   }
   mounted(): void {
     this.getProjectList()
@@ -55,7 +85,9 @@ export default class extends Vue {
 
 <style lang="less" scoped>
 .columnContainer {
-  .border();
+  .border;
+  .radius;
+  .boxShadow;
   .header {
     .center;
     background: @border;
@@ -63,14 +95,20 @@ export default class extends Vue {
     padding: 10px;
   }
   .list {
+    height: 300px;
+    overflow-y: auto;
     .item {
       padding: 7px 10px;
       .borderBottom;
       .trans;
       .pointer;
-      &:hover {
+      &.active {
         background: @green;
         color: @primary;
+      }
+      &:hover {
+        background: @black;
+        color: @green;
       }
       &:last-child {
         .borderNone;
